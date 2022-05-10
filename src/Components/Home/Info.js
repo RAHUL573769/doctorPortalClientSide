1ebdcfd70aeaae954a7b129bb2fbc5cbb3d10a1b
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-10 16:37:13 +0600

    modifications7

--- src/Components/Home/Info.js
+++ src/Components/Home/Info.js
@@ -2,13 +2,14 @@ import React from "react";
 import Infocad from "./Infocad";
 
 import clock from "../../Images/assets/icons/clock.svg";
+import image2 from "../../Images/assets/icons/marker.svg";
 
 const Info = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-      <Infocad img={clock}></Infocad>
-      <Infocad img={clock}></Infocad>
-      <Infocad img={clock}></Infocad>
+      <Infocad cardTitle="Opening Hours" img={clock}></Infocad>
+      <Infocad cardTitle="Our Location" img={image2}></Infocad>
+      <Infocad cardTitle="Contact Us" img={clock}></Infocad>
     </div>
   );
 };
--- src/Components/Home/Infocad.js
+++ src/Components/Home/Infocad.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Infocad = ({ img }) => {
+const Infocad = ({ img, cardTitle }) => {
   return (
     <div>
-      <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
+      <div class="card lg:card-side bg-base-100 shadow-xl bg-accent px-12">
         <figure>
           <img src={img} />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">New album is released!</h2>
+          <h2 class="card-title"> {cardTitle}</h2>
           <p>Click the button to listen on Spotiwhy app.</p>
         </div>
       </div>

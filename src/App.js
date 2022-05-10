f7b5e05d94cef100f9900b726a68e535dee76600
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-10 15:40:04 +0600

    modifications3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./Components/About/About";
+import Appointmenr from "./Components/Appointment/Appointmenr";
+import Contact from "./Components/Contact/Contact";
 import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Review from "./Components/Review/Review";
 import Header from "./Components/Shared/Header";
 
 function App() {
@@ -11,6 +15,13 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route
+          path="/appointment"
+          element={<Appointmenr></Appointmenr>}
+        ></Route>
       </Routes>
     </div>
   );

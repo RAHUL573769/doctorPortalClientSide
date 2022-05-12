8ecbb30ad5acc5efbe11b7505ed99a0346bcd073
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-12 11:10:45 +0600

    packges32 added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -24,30 +24,74 @@ const Login = () => {
           <h2 className="card-title text-center ">Login</h2>
 
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div class="form-control w-full max-w-xs">
-              <label class="label">
-                <span class="label-text">Email?</span>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email?</span>
               </label>
               <input
                 type="email"
                 placeholder="Email Here"
-                class="input input-bordered w-full max-w-xs"
+                className="input input-bordered w-full max-w-xs"
                 {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is Required",
+                  },
                   pattern: {
                     value: /[A-Za-z]{3}/,
-                    message: "error message", // JS only: <p>error message</p> TS only support string
+                    message: "Please provide valid Email", // JS only: <p>error message</p> TS only support string
                   },
                 })}
               />
+
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}{" "}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}{" "}
+                  </span>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password?</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Password Here"
+                className="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password  is Required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must be 6 characters long", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
+              />
+
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}{" "}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}{" "}
+                  </span>
+                )}
+              </label>
             </div>
 
-            <input />
-            {errors.firstName?.type === "required" && "First name is required"}
-
-            <input {...register("lastName", { required: true })} />
-            {errors.lastName && "Last name is required"}
-
-            <input type="submit" />
+            <input className="btn w-full max-w-xs" type="submit" />
           </form>
 
           <div className="divider">OR</div>
